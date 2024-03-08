b4c26c346149a4c4f2a0ce584391c766ea3a6517
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-03-08 08:45:39 -0500

    Conversor EuroDolar

---
+++ ConversorEuroDolar/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        ConversorEuroDolar obj = new ConversorEuroDolar();
+
+        obj.cambiaValorEuro(1.05);
+
+        Console.WriteLine(obj.Convierte(50));
+
+    }
+}
+class ConversorEuroDolar
+{
+
+    private double euro = 1.253;
+
+    public double Convierte(double cantidad)
+    {
+
+
+        return cantidad * euro;
+    }
+    public void cambiaValorEuro(double nuevoValor)
+    {
+        if (nuevoValor < 0) euro = 1.253;
+        else euro = nuevoValor;
+    
+        euro = nuevoValor;
+    
+    }
+}
