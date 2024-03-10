550674dc465f7adee633669cfcdc20e1e874998a
JOSE LEONARDO MANRIQUE VALLEJO <josemanrique5433@gmail.com>
2024-03-10 10:02:52 -0500

    usos getter setter this

--- UsoCoches/Program.cs
+++ UsoCoches/Program.cs
@@ -16,10 +16,15 @@
 
         Coche coche3 = new Coche(4500.25,1200.35);
         Console.WriteLine(coche3.getInfoCoche());
+        coche3.setExtras(true, "Cuero");
+        Console.WriteLine(coche3.getExtras());
+       
     }
 
-    class Coche
+    partial class Coche
     {
+
+
         //los comportamientos se determinan con las funciones o Métodos
         //Que es un contructor: cual es su comportamiento, su estado inicial, 
         //creamos un constructor:
@@ -30,9 +35,11 @@
             ruedas = 4;
             largo = 2300.5;
             ancho = 0.800;
+            tapiceria = "Tela";
 
 
         }
+    
 
         public Coche(double largoCoche, double anchoCoche)//Sobre carga de constructor. 
         {
@@ -40,22 +47,37 @@
             ruedas = 4;
             largo = largoCoche;
             ancho = anchoCoche;
-
+            tapiceria = "Tela";
 
         }
-        //declaramos un metodo de acceso para que sea visible el constructor Coche.
-        public String getInfoCoche()
-        {
+        
+
+    }
+
+    partial class Coche
+    {
+          
+    public String getInfoCoche() //declaramos un metodo de acceso para que sea visible el constructor Coche.
+    {
             return "Información del coche: \n " + " Ruedas: " + ruedas + " Largo: " + largo + " Ancho: " + ancho;
 
         }
 
-        public void setExtras(bool paramClimatizador, String paramTapiceria) //Primer metodo Setter
+        public void setExtras(bool Climatizador, String Tapiceria) //Primer metodo Setter
         {
 
-            climatizador = paramClimatizador;
-            tapiceria = paramTapiceria;
+            this.climatizador = Climatizador;
+            this.tapiceria = Tapiceria;
 
+
+        }
+
+        public String getExtras()
+        {
+
+
+
+            return "Extras del Coche: \n" + " Tapiceria: " + tapiceria + " Climatizador: " + climatizador; 
 
         }
 
